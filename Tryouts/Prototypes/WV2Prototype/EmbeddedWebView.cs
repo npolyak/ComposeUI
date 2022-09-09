b1e2a48173009056d2df36e7f421c5abb05e2749
Nick Polyak <nick.polyak@morganstanley.com>
2022-09-09 16:56:19 -0400

    created a first WebView2 sample

---
+++ Tryouts/Prototypes/WV2Prototype/EmbeddedWebView.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia.Controls;
+using Avalonia.Platform;
+using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Threading.Tasks;
+
+namespace MorganStanley.ComposeUI.Prototypes.WV2Prototype
+{
+    public class EmbeddedWebView : NativeControlHost
+    {
+        WebView2 _webView;
+        public EmbeddedWebView()
+        {
+            _webView = new WebView2();
+
+            _ = Init();
+        }
+
+        public async Task Init()
+        {
+            await _webView.EnsureCoreWebView2Async(null);
+            _webView.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+        }
+
+        private void CoreWebView2_WebMessageReceived
+        (
+            object? sender,
+            Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs e)
+        {
+
+        }
+
+        public Uri? Source
+        {
+            get => _webView.Source;
+            set => _webView.Source = value;
+        }
+
+
+        protected override IPlatformHandle CreateNativeControlCore(IPlatformHandle parent)
+        {
+            IPlatformHandle handle =
+                new PlatformHandle(_webView.Handle, "HWND");
+
+            return handle;
+        }
+
+        protected override void DestroyNativeControlCore(IPlatformHandle control)
+        {
+            _webView.Dispose();
+        }
+    }
+}
